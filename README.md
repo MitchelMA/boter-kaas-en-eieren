a1b3686733ac892adb18b49fb7b61c65ea7c05ca
MitchelMA <32548@ma-web.nl>
2022-02-05 20:15:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boter Kaas en Eieren
+Bit is boter kaas en eieren in c++
+
+## Benodigdheden
+Een compiler  
+Op deze manier kan je een .exe bestand maken van [game.cpp](game.cpp)  
+  ***
+Dit programma is helaas alleen geschreven voor windows apparaten voor zover mij bekend,  
+ik heb het niet gecheckt op een ander OS.
---
+++ game.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char bord[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+char pieces[2] = {'O', 'X'};
+int beurt = 1;
+
+void drawBord();
+void invoerhandel(int plaatsindex);
+void beurtStart();
+typedef struct
+{
+    bool einde;
+    char PieceIsWinnaar;
+} uitkomst;
+uitkomst checkField();
+
+int main()
+{
+    beurtStart();
+    return 0;
+}
+
+// functie om het bord te tekenen
+void drawBord()
+{
+    cout << bord[0] << " | " << bord[1] << " | " << bord[2] << endl;
+    cout << "---------" << endl;
+    cout << bord[3] << " | " << bord[4] << " | " << bord[5] << endl;
+    cout << "---------" << endl;
+    cout << bord[6] << " | " << bord[7] << " | " << bord[8] << endl;
+    cout << endl;
+}
+
+void beurtStart()
+{
+    // Teken het bord
+    drawBord();
+    cout << "Speler " << pieces[beurt % 2] << " is nu aan de beurt" << endl;
+    cout << "Op welke plek wil je een \"" << pieces[beurt % 2] << "\" plaatsen? (Voer een getal van 1 - 9 in.)" << endl;
+
+    int invoer = 0;
+    cin >> invoer;
+    cin.get();
+    invoerhandel(invoer);
+}
+
+void invoerhandel(int plaatsindex)
+{
+    // zorg dat de plaatsindex binnen het domein [1, 9] blijft
+    if (plaatsindex < 1 || plaatsindex > 9)
+    {
+        cout << plaatsindex << " is geen geldige invoer. vul een nieuwe plek in tussen 1 en 9" << endl;
+        int invoer = 1;
+        cout << "invoer: " << invoer << endl;
+        cin >> invoer;
+        cin.get();
+        cout << "invoer: " << invoer << endl;
+        invoerhandel(invoer);
+
+        return;
+    }
+
+    // zorg dat je niet op een plek kan plaatsen waar al iets staat
+    if (bord[plaatsindex - 1] != ' ')
+    {
+        cout << "Deze plek is al bezet door " << bord[plaatsindex - 1] << ". Voer een nieuwe plek in:" << endl;
+
+        int invoer = 1;
+        cin >> invoer;
+        cin.get();
+        invoerhandel(invoer);
+
+        return;
+    }
+
+    // zet de keuze van de speler
+    bord[plaatsindex - 1] = pieces[beurt % 2];
+
+    // check na iedere beurt het speelveld;
+    uitkomst check = checkField();
+    if (check.einde)
+    {
+        if ((bool)(int)check.PieceIsWinnaar)
+            cout << check.PieceIsWinnaar << " is de winnaar!" << endl;
+        else
+            cout << "Er is geen winnaar, het spel is afgelopen" << endl;
+        drawBord();
+        return;
+    }
+
+    // ga door naar de volgende beurt
+    beurt++;
+    beurtStart();
+}
+
+uitkomst checkField()
+{
+    bool winnaar = false;
+    char speler = 0;
+
+    // check horizontaal
+    for (int column = 0; column < 3; column++)
+    {
+        string columnString = "";
+        columnString += bord[3 * column];
+        columnString += bord[3 * column + 1];
+        columnString += bord[3 * column + 2];
+
+        if (columnString == "OOO") // O wint
+        {
+            winnaar = true;
+            speler = 'O';
+            return uitkomst{
+                winnaar,
+                speler,
+            };
+        }
+        else if (columnString == "XXX")
+        {
+            winnaar = true;
+            speler = 'X';
+            return uitkomst{
+                winnaar,
+                speler,
+            };
+        }
+    }
+
+    // check verticaal
+    for (int row = 0; row < 3; row++)
+    {
+        string rowString = "";
+        rowString += bord[0 + row];
+        rowString += bord[3 + row];
+        rowString += bord[6 + row];
+
+        if (rowString == "OOO")
+        {
+            winnaar = true;
+            speler = 'O';
+            return uitkomst{
+                winnaar,
+                speler,
+            };
+        }
+        else if (rowString == "XXX")
+        {
+            winnaar = true;
+            speler = 'X';
+            return uitkomst{
+                winnaar,
+                speler,
+            };
+        }
+    }
+
+    // check dioganaal (linksboven naar rechtsonder)
+    if (bord[0] == 'O' && bord[4] == 'O' && bord[8] == 'O')
+    {
+        winnaar = true;
+        speler = 'O';
+        return uitkomst{
+            winnaar,
+            speler,
+        };
+    }
+    else if (bord[0] == 'X' && bord[4] == 'X' && bord[8] == 'X')
+    {
+        winnaar = true;
+        speler = 'X';
+        return uitkomst{
+            winnaar,
+            speler,
+        };
+    }
+
+    // check dioganaal (rechtsboven naar linksonder)
+    if (bord[2] == 'O' && bord[4] == 'O' && bord[6] == 'O')
+    {
+        winnaar = true;
+        speler = 'O';
+        return uitkomst{
+            winnaar,
+            speler,
+        };
+    }
+    else if (bord[2] == 'X' && bord[4] == 'X' && bord[6] == 'X')
+    {
+        winnaar = true;
+        speler = 'X';
+        return uitkomst{
+            winnaar,
+            speler,
+        };
+    }
+
+    // check of het bord vol is
+    bool boolArr[9];
+    for (int i = 0; i < 9; i++)
+    {
+        boolArr[i] = (bool)((int)bord[i] != 32);
+    }
+    int i = 0;
+    while (i < 9)
+    {
+        if (boolArr[i] == false)
+            break;
+        i++;
+    }
+
+    // 9 valt buiten de matrix, dus dan is het hele bord vol
+    if (i > 8)
+    {
+        return uitkomst{
+            true,
+            0,
+        };
+    }
+
+    // standaard uitkomst
+    uitkomst stand = uitkomst{
+        false,
+        0,
+    };
+    return stand;
+}
